2a8e392607817cdebad1aa31ee8bf5bfb239df4f
Sam Smith <sam@mintcanary.com>
2014-05-11 12:56:38 +0100

    new layout

--- js/main.js
+++ js/main.js
@@ -1,56 +1,4 @@
-//Give classes based on scroll position
-$('.dark').waypoint(function(direction) {
-  if (direction === 'down') {
-    $('body').addClass('darkness');
-  }
-  else {
-    $('body').removeClass('darkness');
-  }
-});
-
-$('.money.top').waypoint(function(direction) {
-  $('body').addClass('scrolled');
-});
-
-
-$('.form').waypoint(function(direction) {
-  if (direction === 'down') {
-    $('.form').addClass('stuck');
-  }
-  else {
-    $('.form').removeClass('stuck');
-  }
-}, 
-{ 
-  offset: 70 
-});
-
 jQuery(document).ready(function ($) {
-	
-	//initialise Stellar.js		
-		$(window).stellar({
-			horizontalScrolling: false,
-			hideDistantElements: false, //to fix problem with cached pages
-		});
-
-	
-	//Cache some variables
-			htmlbody = $('html,body')
-	
-	//Create a function that will be passed a slide number and then will scroll to that slide using jquerys animate. The Jquery
-			//easing plugin is also used, so we passed in the easing method of 'easeInOutQuint' which is available throught the plugin.
-			function goToByScroll(dataslide) {
-					htmlbody.animate({
-							scrollTop: $('.slide[data-slide="' + dataslide + '"]').offset().top
-					}, 2000, 'easeInOutQuint');
-			}
-	
-	//When the user clicks on the navigation links, get the data-slide attribute value of the link and pass that variable to the goToByScroll function
-	$( "a.scrollto" ).click(function (e) {
-			e.preventDefault();
-			dataslide = $(this).attr('data-slide');
-			goToByScroll(dataslide);
-	});
 	
 	//Thanks page
 	function getQueryVariable(variable)
@@ -68,7 +16,9 @@ jQuery(document).ready(function ($) {
 	var modal = getQueryVariable('page');
 	
 	if (redirect === 'signed') {
-    $('html').addClass('signed');
+    $('#thanksModal').modal({
+      show: true
+    })
   }
 
   //get Involved
@@ -82,10 +32,4 @@ jQuery(document).ready(function ($) {
 		window.location = '?page=get-involved';
 	});
 	
-});
-
-// Placeholder pollyfill
-Modernizr.load({
-    test: Modernizr.input.placeholder,
-    nope: 'js/vendor/placeholder/src/simple.js'
 });
